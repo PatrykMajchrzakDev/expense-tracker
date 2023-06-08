8984fe3e5bcdc356f751f6f49e6cadf10bc979db
PatrykMajchrzakDev <patrykmajchrzakdev@gmail.com>
2023-06-08 21:09:59 +0200

    Moved stuff to separate folders to make things more organized

---
+++ src/components/Expenses/ExpenseItem.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+const ExpenseItem = (expenses) => {
+  // console.log(expenses);
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={expenses.date} />
+      <div className="expense-item__description">
+        <h2>{expenses.title}</h2>
+        <div className="expense-item__price">{expenses.amount}zł</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
